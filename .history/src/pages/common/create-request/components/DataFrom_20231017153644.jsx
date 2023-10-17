225b57fa2b6f8c23a7301b2b3d84b020d6bba0fc
DuongBT153717 <104844625+DuongBT153717@users.noreply.github.com>
2023-10-17 20:41:10 +0700

    Book Room

---
+++ .history/src/pages/common/create-request/components/DataFrom_20231017153644.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Box, Button, Grid, MenuItem, Paper, Select, TextField, Typography, useTheme } from '@mui/material'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import dayjs from 'dayjs';
+
+const AttendenceFrom = () => (
+  <Box p={3}>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography fontWeight="700" fontSize="18px">
+          Request details{' '}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography fontWeight="500">Title</Typography>
+        <TextField sx={{ width: '100%' }} size="small" placeholder="Enter the request title" />
+      </Grid>
+      <Grid item xs={5}>
+        <Typography fontWeight="500">Position</Typography>
+        <Select
+          sx={{ width: '100%', height: '56px' }}
+          displayEmpty>
+          <MenuItem value="">
+            <em>HR</em>
+          </MenuItem>
+          <MenuItem value={20}>Manager</MenuItem>
+        </Select>
+      </Grid>
+      <Grid item xs={3}>
+        <Typography fontWeight="500">From</Typography>
+        <TextField
+         sx={{ width: '100%', height: '38px' }}
+        type={"time"}
+      />
+      </Grid>
+      <Grid item xs={3}>
+        <Typography fontWeight="500">To</Typography>
+        <TextField
+         sx={{ width: '100%', height: '38px' }}
+        type={"time"}
+      />
+      </Grid>
+      <Grid item xs={12}>
+        <Typography fontWeight="500">Content</Typography>
+        <CKEditor editor={ClassicEditor} onInit={() => { }} />
+      </Grid>
+    </Grid>
+    <Box pt={2} display="flex" alignItems="flex-end" justifyContent="flex-end">
+      <Button variant="contained">Save</Button>
+    </Box>
+  </Box>
+);
+
+const RoomRequestForm = () => (
+  <Box p={3}>
+  <Grid container spacing={2}>
+    <Grid item xs={12}>
+      <Typography fontWeight="700" fontSize="18px">
+        Request details{' '}
+      </Typography>
+    </Grid>
+    <Grid item xs={12}>
+      <Typography fontWeight="500">Title</Typography>
+      <TextField sx={{ width: '100%' }} size="small" placeholder="Enter the request title" />
+    </Grid>
+
+    <Grid item xs={3}>
+      <Typography fontWeight="500">From</Typography>
+      <TextField
+       sx={{ width: '100%', height: '38px' }}
+      type={"time"}
+    />
+    </Grid>
+    <Grid item xs={3}>
+      <Typography fontWeight="500">To</Typography>
+      <TextField
+       sx={{ width: '100%', height: '38px' }}
+      type={"time"}
+    />
+    </Grid>
+    <Grid item xs={12}>
+      <Typography fontWeight="500">Content</Typography>
+      <CKEditor editor={ClassicEditor} onInit={() => { }} />
+    </Grid>
+  </Grid>
+  <Box pt={2} display="flex" alignItems="flex-end" justifyContent="flex-end">
+    <Button variant="contained">Save</Button>
+  </Box>
+</Box>
+);
+
+const OtRequest = () => (
+  <Box p={3}>
+    Ot Request From
+  </Box>
+);
+
+const DepartmentRequest = () => (
+  <Box p={3}>
+    Department Request From
+  </Box>
+);
+
+
+const LeaveRequest = () => (
+  <Box p={3}>
+    Leave Request
+  </Box>
+);
+
+export { AttendenceFrom, RoomRequestForm, LeaveRequest, DepartmentRequest, OtRequest };
