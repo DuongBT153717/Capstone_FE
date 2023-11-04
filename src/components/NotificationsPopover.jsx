32278a5cc893d60e1c05d545fafc9ad650b9c8ee
thinhnqhe150498 <105189247+thinhnqhe150498@users.noreply.github.com>
2023-11-04 16:01:19 +0700

    thinh commit ListAllNotification

--- src/components/NotificationsPopover.jsx
+++ src/components/NotificationsPopover.jsx
@@ -1,17 +1,21 @@
 import { Badge, Button, Box, Divider, IconButton, List, ListItemButton, ListItemText, ListSubheader, Popover, Typography } from '@mui/material';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import CampaignIcon from '@mui/icons-material/Campaign'; // Import CampaignIcon
 import axiosClient from '../utils/axios-config';
 import { toast } from 'react-toastify';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
 import { BASE_URL } from '../services/constraint';
+import { format } from 'date-fns';
 
 const NotificationsPopover = (props) => {
-  const { row } = props
+  const { row } = props;
   const [open, setOpen] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [listNotifications, setListNotifications] = useState([]);
+  const [showMore, setShowMore] = useState(false);
+  const [viewAll, setViewAll] = useState(false);
+
   const userId = useSelector((state) => state.auth.login.currentUser.accountId);
 
   const handleOpen = (event) => {
@@ -20,6 +24,13 @@ const NotificationsPopover = (props) => {
 
   const handleClose = () => {
     setOpen(null);
+  }
+
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.slice(0, maxLength) + '...';
+    }
+    return text;
   }
 
   useEffect(() => {
@@ -48,11 +59,20 @@ const NotificationsPopover = (props) => {
     fetchAllNotifications();
   }, [userId]);
 
+  const sortedNotifications = listNotifications.notifications
+    ? listNotifications.notifications
+      .slice()
+      .sort((a, b) => (a.readStatus === b.readStatus ? 0 : a.readStatus ? 1 : -1))
+      .slice(0, viewAll ? listNotifications.notifications.length : (showMore ? listNotifications.notifications.length : 5))
+    : [];
+
+  const newNotifications = sortedNotifications.filter(notification => !notification.readStatus);
+  const previousNotifications = sortedNotifications.filter(notification => notification.readStatus);
 
   return (
     <>
       <IconButton sx={{ color: 'rgb(94, 53, 177)' }} onClick={handleOpen}>
-        <Badge color="error">
+        <Badge badgeContent={newNotifications.length} color="error">
           <NotificationsIcon />
         </Badge>
       </IconButton>
@@ -63,42 +83,175 @@ const NotificationsPopover = (props) => {
         onClose={handleClose}
         PaperProps={{
           elevation: 0,
-          sx: {}
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            width: 360,
+            maxHeight: '400px',
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0
+            },
+            overflowY: 'auto',
+          }
         }}
         transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}>
-    <Box sx={{ display: 'flex', alignItems: 'center', py: 1, px: 2.5 }}>
-    <Box sx={{ flexGrow: 1 }}>
-      <Typography variant="subtitle1">Notifications</Typography>
-      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-      
-      </Typography>
-    </Box>
-  </Box>
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <Box sx={{ display: 'flex', alignItems: 'center', py: 1, px: 2.5 }}>
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="subtitle1">Notifications</Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              You have {listNotifications.total} notifications
+              {newNotifications.length > 0 && (
+                <span> ({newNotifications.length} unread)</span>
+              )}
+            </Typography>
+          </Box>
+        </Box>
         <Divider sx={{ borderStyle: 'dashed' }} />
         <List disablePadding>
-          <ListSubheader
-            disableSticky
-            sx={{ px: 2.5, py: 1, typography: 'overline', fontSize: '13px', lineHeight: '1.5' }}>
-            New
-          </ListSubheader>
-          {listNotifications?.notifications?.length > 0 ? (
-            listNotifications.notifications.map((notifications_row) => (
-              <ListItemButton key={notifications_row.notificationId} sx={{ px: 2.5, pb: 1, display: 'flex', gap: '10px' }}>
-                <ListItemText primary={notifications_row.title} />
-                <ListItemText primary={notifications_row.uploadDate} />
-              </ListItemButton>
-            ))
-          ) : (
-            <ListItemButton sx={{ px: 2.5, pb: 1, display: 'flex', gap: '10px' }}>
-              <ListItemText primary="No notifications found" />
-            </ListItemButton>
+          {newNotifications.length > 0 && (
+            <>
+              <ListSubheader
+                disableSticky
+                sx={{
+                  px: 2.5,
+                  py: 1,
+                  typography: 'overline',
+                  fontSize: '13px',
+                  lineHeight: '1.5',
+                  fontWeight: 'bold',
+                  textTransform: 'uppercase',
+                  color: 'rgb(94, 53, 177)'
+                }}
+              >
+                New
+              </ListSubheader>
+              {newNotifications.map((notification) => (
+                <ListItemButton
+                  key={notification.notificationId}
+                  sx={{
+                    px: 2.5,
+                    pb: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '5px',
+                    backgroundColor: 'lightgray',
+                    alignItems: 'flex-start',
+                  }}
+                >
+                  <Typography variant="body2" color="text.secondary" sx={{ fontStyle: 'italic', fontSize: '0.9rem' }}>
+                    from {notification.department.departmentName}
+                  </Typography>
+                  <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <ListItemText
+                      primary={truncateText(notification.title, 30)}
+                      sx={{ fontSize: '2.5rem', fontWeight: 'bold' }}
+                    />
+                    {notification.readStatus ? null : (
+                      <CampaignIcon
+                        fontSize="large"
+                        sx={{ color: '#1976d2' }}
+                      />
+                    )}
+                  </Box>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    sx={{
+                      fontSize: '0.9rem',
+                      fontStyle: 'italic',
+                      color: 'grey'
+                    }}
+                  >
+                    {format(new Date(notification.uploadDate), 'yy/MM/dd HH:mm:ss')}
+                  </Typography>
+                </ListItemButton>
+              ))}
+            </>
+          )}
+
+          {previousNotifications.length > 0 && (
+            <>
+              <ListSubheader
+                disableSticky
+                sx={{
+                  px: 2.5,
+                  py: 1,
+                  typography: 'overline',
+                  fontSize: '13px',
+                  lineHeight: '1.5',
+                  fontWeight: 'bold',
+                  textTransform: 'uppercase',
+                  color: 'rgb(94, 53, 177)'
+                }}
+              >
+                Previous
+              </ListSubheader>
+              {previousNotifications.map((notification) => (
+                <ListItemButton
+                  key={notification.notificationId}
+                  sx={{
+                    px: 2.5,
+                    pb: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '5px',
+                    backgroundColor: 'transparent',
+                    alignItems: 'flex-start',
+                  }}
+                >
+                  <Typography variant="body2" color="text.secondary" sx={{ fontStyle: 'italic', fontSize: '0.9rem' }}>
+                    from {notification.department.departmentName}
+                  </Typography>
+                  <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <ListItemText
+                      primary={truncateText(notification.title, 30)}
+                      sx={{ fontSize: '2.5rem', fontWeight: 'bold' }}
+                    />
+                    {notification.readStatus ? null : (
+                      <CampaignIcon
+                        fontSize="large"
+                        sx={{ color: 'deepskyblue' }}
+                      />
+                    )}
+                  </Box>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    sx={{
+                      fontSize: '0.9rem',
+                      fontStyle: 'italic',
+                      color: 'grey'
+                    }}
+                  >
+                    {format(new Date(notification.uploadDate), 'yy/MM/dd HH:mm:ss')}
+                  </Typography>
+                </ListItemButton>
+              ))}
+            </>
           )}
         </List>
         <Divider sx={{ borderStyle: 'dashed' }} />
         <Box sx={{ p: 1 }}>
-          <Button fullWidth disableRipple disablePadding>
-            View All
+          <Button fullWidth disableRipple disablePadding onClick={() => setViewAll(!viewAll)}>
+            {viewAll ? 'Show Less' : 'View More'}
           </Button>
         </Box>
       </Popover>
