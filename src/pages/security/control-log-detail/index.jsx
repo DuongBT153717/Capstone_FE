44cd2ee4c626baf1fa24846bef48ac73acf33d63
thinhnqhe150498 <105189247+thinhnqhe150498@users.noreply.github.com>
2023-12-09 17:02:39 +0700

    thinh fix bug manage User by hr

--- src/pages/security/control-log-detail/index.jsx
+++ src/pages/security/control-log-detail/index.jsx
@@ -1,4 +1,4 @@
-import  { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import securityApi from '../../../services/securityApi';
 import { Avatar, Box, Divider, Typography } from '@mui/material';
@@ -11,8 +11,8 @@ const ControlLogDetail = () => {
     console.log(username);
     console.log(controlLogId);
     const [control, setControl] = useState()
-    const [deviceImg,setDeviceImg] = useState("")
-    const [AvatarImg,setAvatarImg] = useState("")
+    const [deviceImg, setDeviceImg] = useState("")
+    const [AvatarImg, setAvatarImg] = useState("")
 
     useEffect(() => {
         const getControlLogDetail = async () => {
@@ -25,28 +25,28 @@ const ControlLogDetail = () => {
     const imgurl = async () => {
         const storageRef = ref(storage, `/${control?.avatar}`)
         try {
-          const url = await getDownloadURL(storageRef)
-          setAvatarImg(url)
+            const url = await getDownloadURL(storageRef)
+            setAvatarImg(url)
         } catch (error) {
-          console.error('Error getting download URL:', error)
+            console.error('Error getting download URL:', error)
         }
-      }
-      const imgurldevice = async () => {
+    }
+    const imgurldevice = async () => {
         const storageRef = ref(storage, `/${control?.image}`)
         try {
-          const url = await getDownloadURL(storageRef)
-          setDeviceImg(url)
+            const url = await getDownloadURL(storageRef)
+            setDeviceImg(url)
         } catch (error) {
-          console.error('Error getting download URL:', error)
+            console.error('Error getting download URL:', error)
         }
-      }
-      
-      if (control && control?.avatar && control?.image) {
+    }
+
+    if (control && control?.avatar && control?.image) {
         imgurl()
         imgurldevice()
-      }
+    }
 
-   
+
 
     console.log(control);
 
@@ -56,75 +56,75 @@ const ControlLogDetail = () => {
             <Box display='flex' alignItems='center' mb={3}>
                 <Box flex='1' >
                     <Avatar
-                    src={AvatarImg}
+                        src={AvatarImg}
                         sx={{
                             margin: 'auto',
-                            height: 100,
-                            width: 100
+                            height: 140,
+                            width: 140
                         }}
                     />
                 </Box>
                 <Box flex='1' display="flex" marginTop="40px" height='80px'>
                     <Box flex="1" textAlign='left' borderRight="1px solid #999">
-                        <Typography >Account </Typography>
-                        <Typography mt={4}>Role </Typography>
+                        <Typography fontSize={20}>Account: </Typography>
+                        <Typography fontSize={20} mt={4}>Role: </Typography>
                     </Box>
                     <Box flex="2" textAlign='left' marginLeft="20px">
-                        <Typography>{control?.account}</Typography>
-                        <Typography mt={4} sx={{ textTransform: 'capitalize' }}>{control?.role} </Typography>
+                        <Typography fontSize={20}>{control?.account}</Typography>
+                        <Typography fontSize={20} mt={4} sx={{ textTransform: 'capitalize' }}>{control?.role} </Typography>
                     </Box>
                 </Box>
                 <Box flex='1' display="flex" marginTop="40px" height='80px'>
                     <Box flex="1" textAlign='left' borderRight="1px solid #999">
-                        <Typography >Department </Typography>
-                        <Typography mt={4}>Hire DateDate </Typography>
+                        <Typography fontSize={20} >Department </Typography>
+                        <Typography fontSize={20} mt={4}>Hire Date </Typography>
                     </Box>
                     <Box flex="2" textAlign='left' marginLeft="20px">
-                        <Typography sx={{ textTransform: 'capitalize' }}>{control?.department} </Typography>
-                        <Typography mt={4}>{control?.hireDate} </Typography>
+                        <Typography fontSize={20} sx={{ textTransform: 'capitalize' }}>{control?.department} </Typography>
+                        <Typography fontSize={20} mt={4}>{control?.hireDate} </Typography>
                     </Box>
                 </Box>
             </Box>
             <Divider />
             <Box m={5}>
                 <Avatar
-                src={deviceImg}
+                    src={deviceImg}
                     sx={{
                         marginLeft: '135px',
-                        height: 100,
-                        width: 100
+                        height: 140,
+                        width: 140
                     }}
                 />
                 <Box display='flex' marginLeft='100px' >
-                <Box flex='1' display="flex" marginTop="40px" height='220px'>
-                    <Box flex="1" textAlign='left' borderRight="1px solid #999">
-                        <Typography >Device id </Typography>
-                        <Typography mt={4}>Device name </Typography>
-                        <Typography mt={4}>Time </Typography>
-                        <Typography mt={4}>Similar </Typography>
+                    <Box flex='1' display="flex" marginTop="40px" height='220px'>
+                        <Box flex="1" textAlign='left' borderRight="1px solid #999">
+                            <Typography fontSize={20} >Device Id </Typography>
+                            <Typography fontSize={20} mt={4}>Device Name </Typography>
+                            <Typography fontSize={20} mt={4}>Date/Time </Typography>
+                            <Typography fontSize={20} mt={4}>Similar </Typography>
+                        </Box>
+                        <Box flex="2" textAlign='left' marginLeft="20px">
+                            <Typography fontSize={20} sx={{ textTransform: 'capitalize' }}>{control?.deviceId} </Typography>
+                            <Typography fontSize={20} mt={4}>{control?.deviceName} </Typography>
+                            <Typography fontSize={20} mt={4}>{control?.time} </Typography>
+                            <Typography fontSize={20} mt={4}>{control?.similar} % </Typography>
+                        </Box>
                     </Box>
-                    <Box flex="2" textAlign='left' marginLeft="20px">
-                        <Typography sx={{ textTransform: 'capitalize' }}>{control?.deviceId} </Typography>
-                        <Typography mt={4}>{control?.deviceName} </Typography>
-                        <Typography mt={4}>{control?.time} </Typography>
-                        <Typography mt={4}>{control?.similar} </Typography>
-                    </Box>
-                </Box>
 
-                <Box flex='1' display="flex" marginTop="40px" height='220px'>
-                    <Box flex="1" textAlign='left' borderRight="1px solid #999">
-                        <Typography >Operator </Typography>
-                        <Typography mt={4}>Person ID </Typography>
-                        <Typography mt={4}>Verify Status </Typography>
-                        <Typography mt={4}>Temperature </Typography>
+                    <Box flex='1' display="flex" marginTop="40px" height='220px'>
+                        <Box flex="1" textAlign='left' borderRight="1px solid #999">
+                            <Typography fontSize={20} >Operator </Typography>
+                            <Typography fontSize={20} mt={4}>Person ID </Typography>
+                            <Typography fontSize={20} mt={4}>Verify Status </Typography>
+                            <Typography fontSize={20} mt={4}>Temperature </Typography>
+                        </Box>
+                        <Box flex="2" textAlign='left' marginLeft="20px">
+                            <Typography fontSize={20} sx={{ textTransform: 'capitalize' }}>{control?.operator} </Typography>
+                            <Typography fontSize={20} mt={4}>{control?.personId} </Typography>
+                            <Typography fontSize={20} mt={4}>{control?.verifyStatus} </Typography>
+                            <Typography fontSize={20} mt={4}>{control?.temperature} <span>&deg; C</span> </Typography>
+                        </Box>
                     </Box>
-                    <Box flex="2" textAlign='left' marginLeft="20px">
-                        <Typography sx={{ textTransform: 'capitalize' }}>{control?.operator} </Typography>
-                        <Typography mt={4}>{control?.personId} </Typography>
-                        <Typography mt={4}>{control?.verifyStatus} </Typography>
-                        <Typography mt={4}>{control?.temperature} </Typography>
-                    </Box>
-                </Box>
                 </Box>
             </Box>
 
